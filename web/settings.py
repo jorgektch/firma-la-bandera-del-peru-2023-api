b2fbd5856cad97c49081827f52d6270bc3774a0b
Jorge Ktch <jorgektch@gmail.com>
2023-07-21 23:39:33 -0500

    with tokens

--- web/settings.py
+++ web/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework.authtoken',
     'rest_framework',
     'signatures.apps.SignaturesConfig',
     'django.contrib.admin',
